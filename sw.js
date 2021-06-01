4338c9d7bf052db66e1005d47d5f6d3e8526a315
Varvus <45293461+Varvus@users.noreply.github.com>
2021-06-01 12:58:13 -0500

    cambios

--- sw.js
+++ sw.js
@@ -1,6 +1,3 @@
-//imports
-/*importScripts("js/sw-utils.js");*/
-
 const STATIC_CACHE = "static-v1";
 const DYNAMIC_CACHE = "dynamic-v1";
 const INMUTABLE_CACHE = "inmutable-v1";
@@ -14,13 +11,13 @@ const APP_SHELL = [
     "style/bg.png",
 ];
 
-const APP_SHELL_INMUTABLE = [
+/*const APP_SHELL_INMUTABLE = [
     "https://fonts.googleapis.com/css?family=Quicksand:300,400",
     "https://fonts.googleapis.com/css?family=Lato:400,300",
     "https://use.fontawesome.com/releases/v5.3.1/css/all.css",
     "css/animate.css",
     "js/libs/jquery.js"
-];
+];*/
 
 self.addEventListener("install", e => {
     const cacheStatic = caches.open( STATIC_CACHE ).then(cache => 
